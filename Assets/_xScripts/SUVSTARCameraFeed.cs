64e08a088bd5a581fcf4c3886c57f0ca00e12d51
dragonkhoi <browninstitute@mair-2.local>
2019-05-28 20:22:59 -0700

    camerafeed test

---
+++ Assets/_xScripts/SUVSTARCameraFeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class SUVSTARCameraFeed : MonoBehaviour
+{
+    public RenderTexture toRender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //toRender = (RenderTexture)Frame.CameraImage.Texture;
+        Graphics.Blit(Frame.CameraImage.Texture, toRender);
+    }
+}
