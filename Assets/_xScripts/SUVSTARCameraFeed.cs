48b1564edf92df01a8f3ce6be19a7060857a8be4
Khoi Le <khoile@stanford.edu>
2019-06-01 15:44:36 -0700

    checkout cardboarddistortion.cginc

--- Assets/_xScripts/SUVSTARCameraFeed.cs
+++ Assets/_xScripts/SUVSTARCameraFeed.cs
@@ -6,10 +6,11 @@ using GoogleARCore;
 public class SUVSTARCameraFeed : MonoBehaviour
 {
     public RenderTexture toRender;
+    Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -17,5 +18,7 @@ public class SUVSTARCameraFeed : MonoBehaviour
     {
         //toRender = (RenderTexture)Frame.CameraImage.Texture;
         Graphics.Blit(Frame.CameraImage.Texture, toRender);
+        rend.material.mainTexture = Frame.CameraImage.Texture;
+
     }
 }
