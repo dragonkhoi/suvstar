29041f5515bad42798535dfd2eb928279b564b56
Khoi Le <khoile@stanford.edu>
2019-06-05 03:06:21 -0700

    time anchored working, 60deg

--- Assets/FollowTransform.cs
+++ Assets/FollowTransform.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class FollowTransform : MonoBehaviour
 {
+    public Transform toFollow;
+    public bool followX;
+    public bool followY;
+    public bool followZ;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class FollowTransform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPosition = new Vector3(); 
+        newPosition.x = followX ? toFollow.position.x : transform.position.x;
+        newPosition.y = followY ? toFollow.position.y : transform.position.y;
+        newPosition.z = followZ ? toFollow.position.z : transform.position.z;
+        transform.position = newPosition;
     }
 }
