ac39d12de560d3f0fbc117c00fad23783ba7a554
Khoi Le <khoile@stanford.edu>
2019-06-05 12:31:02 -0700

    new ui context + pass thru screen cap mode

---
+++ Assets/RaycastUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastUI : MonoBehaviour
+{
+    Transform hitTransform = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 2f))
+        {
+            if (hitTransform != hit.transform)
+            {
+                hit.transform.GetComponent<UIContext>().Expand();
+                hitTransform = hit.transform;
+            }
+        }
+        else
+        {
+            if (hitTransform != null)
+            {
+                hitTransform.GetComponent<UIContext>().Collapse();
+            }
+            hitTransform = null;
+        }
+    }
+}
---
+++ Assets/UIContext.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIContext : MonoBehaviour
+{
+    public GameObject Left;
+    public GameObject Right;
+
+    private Transform currentBg;
+    public Transform rightBg;
+    public Transform leftBg;
+
+    private Transform belowFold;
+    public Transform leftBelowFold;
+    public Transform rightBelowFold;
+
+    private Vector3 ogScale;
+    public Vector3 finalScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentBg = Left.activeInHierarchy ? leftBg : rightBg;
+        ogScale = currentBg.localScale;
+        belowFold = Left.activeInHierarchy ? leftBelowFold : rightBelowFold;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Expand()
+    {
+        StartCoroutine(ExpandUI());
+    }
+
+    IEnumerator ExpandUI()
+    {
+        Vector3 currentScale = transform.localScale;
+        int i = 0;
+        while (i <= 100f)
+        {
+            currentBg.localScale = Vector3.Lerp(currentScale, finalScale, i / 100f);
+            for (int k = 0; k < belowFold.childCount; k++)
+            {
+                belowFold.GetChild(k).GetComponent<TextMesh>().color = Vector4.Lerp(new Vector4(1f,1f,1f,0f), new Vector4(1f, 1f, 1f, 1f), i / 100f);
+            }
+            yield return new WaitForSeconds(0.0005f);
+            i++;
+        } 
+    }
+
+    IEnumerator CollapseUI()
+    {
+        Vector3 currentScale = transform.localScale;
+
+        int i = 0;
+        while (i <= 100f)
+        {
+            currentBg.localScale = Vector3.Lerp(currentScale, ogScale, i / 100f);
+            for (int k = 0; k < belowFold.childCount; k++)
+            {
+                belowFold.GetChild(k).GetComponent<TextMesh>().color = Vector4.Lerp(new Vector4(1f, 1f, 1f, 1f), new Vector4(1f, 1f, 1f, 0f), i / 100f);
+            }
+            yield return new WaitForSeconds(0.0005f);
+            i++;
+        }
+    }
+
+    public void Collapse()
+    {
+        StartCoroutine(CollapseUI());
+    }
+}
