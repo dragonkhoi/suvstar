7f46902f9e5f11daa5eea03a91997657f673df26
Kyle Qian <kylecqian@gmail.com>
2019-06-07 15:43:47 -0700

    Cleaning up

--- Assets/_xScripts/SetTransform.cs
+++ Assets/_xScripts/SetTransform.cs
@@ -1,21 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/* Adjusts EyeCamera Z position based on slider UI. */
 public class SetTransform : MonoBehaviour
 {
     float ogZ;
     public UnityEngine.UI.Text eyeCamZText;
-    // Start is called before the first frame update
+
     void Start()
     {
         ogZ = transform.position.z;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void SetZ(float z)
